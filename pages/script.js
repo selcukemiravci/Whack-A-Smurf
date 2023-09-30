fa51867bd3e74038b83ecced16d7adfe9e633bf9
Jason Osmond <jasonosmond2001@gmail.com>
2023-09-29 23:07:19 -0600

    Smurfs placed
    Smurfs update score when clicked
    smurfs are placed on holes

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,15 @@
 const scoreElement = document.getElementById('score');
-const smurf = document.getElementById('smurf1');
+
+const smurfIds = ['smurf1', 'smurf2', 'smurf3', 'smurf4', 'smurf5', 'smurf6'];
 
 let score = 0;
 
-smurf.addEventListener('click', () => {
-    score++;
-    scoreElement.textContent = score;
-    smurf.style.display = 'none';
+for (const smurfId of smurfIds) {
+    const smurf = document.getElementById(smurfId);
 
-});
+    smurf.addEventListener('click', () => {
+        score++;
+        scoreElement.textContent = score;
+        smurf.style.display = 'none';
+    });
+}
